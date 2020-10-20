f22f1d544cb2479871f611f1479f21e6a9b401b0
cgxarrie <cgxarrie@users.noreply.github.com>
2020-10-20 22:55:09 +0200

    Feature/order by (#9)
    * Add string to linq order by
    
    * Change orderby to extend IQueriable
    
    * Add extension method for Where
    
    * Change where to Extension
    
    * Change create clauses to startc create
    
    * Add Order by clauses
    
    * Add orderby tests

---
+++ LinqUtils/LinqUtils/OrderBy/Clauses/OrderByClausePart.cs
@@ -0,0 +1,10 @@
+﻿namespace csOdin.LinqUtils.OrderBy.Clauses
+{
+    internal abstract class OrderByClausePart
+    {
+        public abstract string AscDesc { get; }
+        public string PropertyName { get; internal set; }
+
+        public override string ToString() => $"{PropertyName} {AscDesc}";
+    }
+}
--- LinqUtils/LinqUtilsTests/OrderByTests/OrderByTests.cs
+++ LinqUtils/LinqUtilsTests/OrderByTests/OrderByTests.cs
@@ -1,24 +1,66 @@
 ﻿namespace csOdin.LinqUtils.Tests.OrderByTests
 {
     using csOdin.LinqUtils.OrderBy;
+    using csOdin.LinqUtils.OrderBy.Clauses;
+    using csOdin.LinqUtils.Tests.Models;
+    using FluentAssertions;
     using System.Linq;
     using Xunit;
 
     public class OrderByTests
     {
         [Theory]
-        [InlineData("Name")]
-        [InlineData("Name desc")]
-        [InlineData("Address.City, Name desc")]
-        [InlineData("Address.City desc, Name")]
-        [InlineData("Address.City desc, Name desc")]
         [InlineData("")]
         [InlineData(null)]
-        public void StringShouldReturn(string inputString)
+        public void EmptyOrNullStringShouldReturnNull(string inputString)
         {
             var people = Models.DummyData.GetPeople().AsQueryable();
 
             var orderedPeople = people.OrderBy(inputString);
+
+            orderedPeople.Should().BeNull();
+        }
+
+        [Fact]
+        public void OrderByClauseShouldReturn()
+        {
+            var people = Models.DummyData.GetPeople().AsQueryable();
+
+            var orderByClause = OrderByClause<Person>.Create()
+                .AddAscending(o => o.Name)
+                .AddDescending("Address.City")
+                .AddAscending(o => o.IntProperty);
+
+            var orderedPeople = people.OrderBy(orderByClause);
+
+            orderedPeople.Expression.ToString().Should().Contain("OrderBy(x => x.Name).ThenByDescending(x => x.Address.City).ThenBy(x => x.IntProperty)");
+        }
+
+        [Fact]
+        public void OrderByClauseShouldReturnNull()
+        {
+            var people = Models.DummyData.GetPeople().AsQueryable();
+
+            var orderByClause = OrderByClause<Person>.Create();
+
+            var orderedPeople = people.OrderBy(orderByClause);
+
+            orderedPeople.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("Name", "OrderBy(x => x.Name)")]
+        [InlineData("Name desc", "OrderByDescending(x => x.Name)")]
+        [InlineData("Address.City, Name desc", "OrderBy(x => x.Address.City).ThenByDescending(x => x.Name)")]
+        [InlineData("Address.City desc, Name", "OrderByDescending(x => x.Address.City).ThenBy(x => x.Name)")]
+        [InlineData("Address.City desc, Name desc", "OrderByDescending(x => x.Address.City).ThenByDescending(x => x.Name)")]
+        public void StringShouldReturn(string inputString, string expextedOrderByExpression)
+        {
+            var people = Models.DummyData.GetPeople().AsQueryable();
+
+            var orderedPeople = people.OrderBy(inputString);
+
+            orderedPeople.Expression.ToString().Should().Contain(expextedOrderByExpression);
         }
     }
 }
